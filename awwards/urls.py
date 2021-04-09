3901f1f5d539db6a80d6dc39dc169ac7b3518424
Lewis Njagi <lewismaybach@gmail.com>
2021-04-09 15:21:21 +0300

    add profile url

--- awwards/urls.py
+++ awwards/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-#     path('profile/<str:username>',views.profile,name = 'profile'),
+    path('profile/<str:username>',views.profile,name = 'profile'),
 #     path('review/<post>', views.review, name='review'),
 #     path('api/profile/', views.ProfileList.as_view()),
 #     path('api/users/', views.UserList.as_view()),
