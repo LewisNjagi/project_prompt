1af3390c08fdb3a97aaf5b3dd86e498aa8a310d3
Lewis Njagi <lewismaybach@gmail.com>
2021-04-09 15:21:38 +0300

    add review url

--- awwards/urls.py
+++ awwards/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('profile/<str:username>',views.profile,name = 'profile'),
-#     path('review/<post>', views.review, name='review'),
+    path('review/<post>', views.review, name='review'),
 #     path('api/profile/', views.ProfileList.as_view()),
 #     path('api/users/', views.UserList.as_view()),
 #     path('api/posts/', views.PostList.as_view()),
