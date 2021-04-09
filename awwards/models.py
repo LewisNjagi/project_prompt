d92f834e46eb135a276c4cc60e7f50f2bc82f344
Lewis Njagi <lewismaybach@gmail.com>
2021-04-09 15:17:15 +0300

    add post model

--- awwards/models.py
+++ awwards/models.py
@@ -33,41 +33,41 @@ class Profile(models.Model):
     def __str__(self):
         return self.name
 
-# class Rating(models.Model):
-#     rating = ((1, '1'),(2, '2'),(3, '3'),(4, '4'),(5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10'))
-#     design = models.IntegerField(choices=rating)
-#     usability = models.IntegerField(choices=rating)
-#     content = models.IntegerField(choices=rating)
-#     total = models.FloatField(default=0,blank=True)
-#     user = models.ForeignKey('Profile',on_delete=models.CASCADE,related_name='rating',null=True)
-#     post = models.ForeignKey('Post',on_delete=models.CASCADE,related_name='rating',null=True)
+class Rating(models.Model):
+    rating = ((1, '1'),(2, '2'),(3, '3'),(4, '4'),(5, '5'),(6, '6'),(7, '7'),(8, '8'),(9, '9'),(10, '10'))
+    design = models.IntegerField(choices=rating)
+    usability = models.IntegerField(choices=rating)
+    content = models.IntegerField(choices=rating)
+    total = models.FloatField(default=0,blank=True)
+    user = models.ForeignKey('Profile',on_delete=models.CASCADE,related_name='rating',null=True)
+    post = models.ForeignKey('Post',on_delete=models.CASCADE,related_name='rating',null=True)
 
-#     def __str__(self):
-#         return self.total
+    def __str__(self):
+        return self.total
 
-# class Post(models.Model):
-#     user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='post')
-#     image = CloudinaryField('gallery/')
-#     title = models.CharField(max_length=30)
-#     description = models.CharField(max_length=400)
-#     url = models.URLField(max_length=400)
-#     category = models.CharField(max_length=40)
+class Post(models.Model):
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='post')
+    image = CloudinaryField('gallery/')
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=400)
+    url = models.URLField(max_length=400)
+    category = models.CharField(max_length=40)
 
-#     def __str__(self):
-#         return f'{self.title}'
+    def __str__(self):
+        return f'{self.title}'
 
-#     def save_post(self):
-#         self.save()
+    def save_post(self):
+        self.save()
 
-#     @classmethod
-#     def update_post(cls,old,new):
-#         cap = Post.objects.filter(description=old).update(description=new)
-#         return cap
+    @classmethod
+    def update_post(cls,old,new):
+        cap = Post.objects.filter(description=old).update(description=new)
+        return cap
 
-#     @classmethod
-#     def all_objects(cls):
-#         posts = cls.objects.all()
-#         return posts
+    @classmethod
+    def all_objects(cls):
+        posts = cls.objects.all()
+        return posts
 
 # RATE_CHOICES = [
 #     (1, '1'),
